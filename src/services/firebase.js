00091617c2798a435d8c34e378c1927703da80a2
Salomegit <sallygithinji72@gmail.com>
2025-03-19 21:16:16 +0300

    fire base config

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,24 +1,29 @@
+
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from 'firebase/firestore';
-// import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "picstack-auth.firebaseapp.com",
-  projectId: "picstack-auth",
-  storageBucket: "picstack-auth.firebasestorage.app",
-  messagingSenderId: "997787318289",
-  appId: "1:997787318289:web:1708536fa7b1d9164e59b0",
-  measurementId: "G-KQ5ZC91YRB"
+  apiKey: "AIzaSyDGQew7P2diUlDxRZlD2rhmCwdkE4o0dnE",
+  authDomain: "new-photo-deck.firebaseapp.com",
+  projectId: "new-photo-deck",
+  storageBucket: "new-photo-deck.firebasestorage.app",
+  messagingSenderId: "126110616415",
+  appId: "1:126110616415:web:5dfc8a77ed47c7457744ea",
+  measurementId: "G-M58G4HED93"
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+export const analytics = getAnalytics(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
